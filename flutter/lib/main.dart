57a80b4dba9f9cf5c9659eb86b1a6b4b04d48919
kashifahmad4070 <kashif.ahmad@antino.io>
2022-09-10 12:19:58 +0530

    commiy

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -2,11 +2,8 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:mvc_bolierplate_getx/my_app.dart';
-
 import 'feature/log_in/view/login_screen.dart';
 
 Future<void> main() async {
-  runApp(MyApp(
-    child: LoginScreen(),
-  ));
+  runApp(MyApp());
 }
