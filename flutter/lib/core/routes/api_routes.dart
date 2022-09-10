e144bb7cb2b1ff2e73c86a59d20884b43d5b269c
Aditya Arya <aarya@coligomed.com>
2022-09-10 16:09:56 +0530

    Pubspec
    Pubspec

--- flutter/lib/core/routes/api_routes.dart
+++ flutter/lib/core/routes/api_routes.dart
@@ -1,5 +1,6 @@
 class ApiUrl {
-  static String liveBaseURL = '';
+  static String liveBaseURL =
+      'http://544b-2402-3a80-9fa-20bc-2de0-e2df-f913-63ae.ngrok.io';
 
   // static String liveBaseURL = 'https://db79-122-161-92-249.in.ngrok.io';
 
--- flutter/lib/feature/developer_profile/view/developer_profile.dart
+++ flutter/lib/feature/developer_profile/view/developer_profile.dart
@@ -17,7 +17,6 @@ class DeveloperProfileScreen extends StatefulWidget {
 
 class _DeveloperProfileScreenState extends State<DeveloperProfileScreen> {
   @override
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -93,7 +92,7 @@ class _DeveloperProfileScreenState extends State<DeveloperProfileScreen> {
               ),
               const SizedBox(height: 20),
               Container(
-                margin: EdgeInsets.symmetric(horizontal: 20),
+                margin: const EdgeInsets.symmetric(horizontal: 20),
                 child: const Divider(
                   color: AppColors.kgrey300,
                 ),
