abca8fb935f316f622bdb165325f0630fd8c8461
kashifahmad4070 <kashif.ahmad@antino.io>
2022-09-10 11:49:26 +0530

    added git ignore

--- flutter/lib/feature/log_in/view/login_screen.dart
+++ flutter/lib/feature/log_in/view/login_screen.dart
@@ -8,112 +8,109 @@ import 'package:mvc_bolierplate_getx/core/reponsive/SizeConfig.dart';
 import 'package:mvc_bolierplate_getx/core/routes/app_routes.dart';
 import 'package:mvc_bolierplate_getx/core/universal_widgets/form_field_widget.dart';
 import 'package:mvc_bolierplate_getx/core/universal_widgets/form_submit_widget.dart';
+import 'package:mvc_bolierplate_getx/core/universal_widgets/searchable_dropdown.dart';
 import 'package:mvc_bolierplate_getx/core/utilites/utility.dart';
 import 'package:mvc_bolierplate_getx/feature/log_in/controller/login_controller.dart';
-
-import '../../../core/universal_widgets/searchable_dropdown.dart';
 
 class LoginScreen extends StatelessWidget {
   LoginScreen({Key? key}) : super(key: key);
 
-  var loginController = Get.put(LoginController());
+  LoginController loginController = Get.put(LoginController());
 
   String? currentSelectedValue;
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(20 * SizeConfig.heightMultiplier!),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Login',
-              style: AppTextStyle.blackBold40,
-            ),
-            SizedBox(
-              height: 40 * SizeConfig.heightMultiplier!,
-            ),
-            SearchableDropdownWidget(
-              hintText: 'Select Country',
-              currrentSelectedValue: currentSelectedValue,
-              titlesList: const [
-                'India',
-                'Australia',
-                'UAE',
-                'Canada',
-                'Germany',
-                'USA',
-              ],
-            ),
-            SizedBox(
-              height: 20 * SizeConfig.heightMultiplier!,
-            ),
-            FormFieldWidget(
-              hintText: 'Phone Number',
-              textInputType: TextInputType.number,
-            ),
-            SizedBox(
-              height: 20 * SizeConfig.heightMultiplier!,
-            ),
-            FormFieldWidget(
-              hintText: 'Password',
-              obscureCharacter: '*',
-              isObscureText: true,
-            ),
-            SizedBox(
-              height: 10 * SizeConfig.heightMultiplier!,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Text(
-                  'Forgot Password?',
-                  style: AppTextStyle.primaryMedium15,
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 20 * SizeConfig.heightMultiplier!,
-            ),
-            FormSubmitWidget(
-              isDisable: false,
-              title: 'Login',
-              titleStyle: AppTextStyle.whiteMedium16,
-              onTap: () {
-                Navigator.pushNamed(context, RouteName.homeScreen);
-                Utility.showInfoDialog(
-                  message: 'Logged In',
-                  isSuccess: true,
-                );
-              },
-            ),
-            SizedBox(
-              height: 10 * SizeConfig.heightMultiplier!,
-            ),
-            FormSubmitWidget(
-              isDisable: false,
-              title: 'Login with Google',
-              prefixIcon: SvgPicture.asset(
-                ImagePath.googleIcon,
+  Widget build(BuildContext context) => Scaffold(
+        body: Padding(
+          padding: EdgeInsets.all(20 * SizeConfig.heightMultiplier!),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Login',
+                style: AppTextStyle.blackBold40,
               ),
-              buttonColor: AppColors.kPureWhite,
-              titleStyle: AppTextStyle.whiteMedium16
-                  .copyWith(color: AppColors.kPureBlack),
-              onTap: () async {
-                await loginController.googleAuthentication.signIn();
-                // Navigator.pushNamed(context, RouteName.homeScreen);
-                // Utility.showInfoDialog(
-                //   message: 'Logged In',
-                //   isSuccess: true,
-                // );
-              },
-            ),
-          ],
+              SizedBox(
+                height: 40 * SizeConfig.heightMultiplier!,
+              ),
+              SearchableDropdownWidget(
+                hintText: 'Select Country',
+                currrentSelectedValue: currentSelectedValue,
+                titlesList: const [
+                  'India',
+                  'Australia',
+                  'UAE',
+                  'Canada',
+                  'Germany',
+                  'USA',
+                ],
+              ),
+              SizedBox(
+                height: 20 * SizeConfig.heightMultiplier!,
+              ),
+              FormFieldWidget(
+                hintText: 'Phone Number',
+                textInputType: TextInputType.number,
+              ),
+              SizedBox(
+                height: 20 * SizeConfig.heightMultiplier!,
+              ),
+              FormFieldWidget(
+                hintText: 'Password',
+                obscureCharacter: '*',
+                isObscureText: true,
+              ),
+              SizedBox(
+                height: 10 * SizeConfig.heightMultiplier!,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    'Forgot Password?',
+                    style: AppTextStyle.primaryMedium15,
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20 * SizeConfig.heightMultiplier!,
+              ),
+              FormSubmitWidget(
+                isDisable: false,
+                title: 'Login',
+                titleStyle: AppTextStyle.whiteMedium16,
+                onTap: () {
+                  Navigator.pushNamed(context, RouteName.homeScreen);
+                  Utility.showInfoDialog(
+                    message: 'Logged In',
+                    isSuccess: true,
+                  );
+                },
+              ),
+              SizedBox(
+                height: 10 * SizeConfig.heightMultiplier!,
+              ),
+              FormSubmitWidget(
+                isDisable: false,
+                title: 'Login with Google',
+                prefixIcon: SvgPicture.asset(
+                  ImagePath.googleIcon,
+                ),
+                buttonColor: AppColors.kPureWhite,
+                titleStyle: AppTextStyle.whiteMedium16
+                    .copyWith(color: AppColors.kPureBlack),
+                onTap: () async {
+                  await loginController.googleAuthentication.signIn();
+                  // Navigator.pushNamed(context, RouteName.homeScreen);
+                  // Utility.showInfoDialog(
+                  //   message: 'Logged In',
+                  //   isSuccess: true,
+                  // );
+                },
+              ),
+            ],
+          ),
         ),
-      ),
-    );
-  }
+      );
 }
